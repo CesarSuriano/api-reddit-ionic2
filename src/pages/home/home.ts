de087bbcb38aeb6a82d1bd8948f09bdd5f044fcf
César Augusto <cesaraugustokg6@hotmail.com>
2017-01-01 00:25:02 -0200

    imagem padrão para as células

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, LoadingController } from 'ionic-angular';
 import { Http } from '@angular/http';
+import { InAppBrowser } from 'ionic-native';
 import 'rxjs/add/operator/map';
 
 
@@ -9,7 +10,7 @@ import 'rxjs/add/operator/map';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public feeds: Array<string>;
+  public feeds: Array<any>;
   private url: string = "https://www.reddit.com/new.json"
 
   constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
@@ -26,15 +27,20 @@ export class HomePage {
   
     this.http.get(this.url).map(res => res.json())
         .subscribe(data => {
-          this.feeds = data.data.children;
-  
+          this.feeds = data.data.children; 
+
+          this.feeds.forEach((e, i, a) => {
+            if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ) { 
+              e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+            }
+          }) 
           loading.dismiss();
         });
   
   }
 
   itemSelected (feed):void {
-    alert(feed.data.url);
+    let browser = new InAppBrowser(this.url, 'blank');
   } 
 
 }
